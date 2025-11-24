0427fd4dbd2a4ff4e57636b938a4155b0f900a6c
yorichill <thomas.haenel101@gmail.com>
2025-11-24 17:49:05 +0100

    ajout de putchar pour printf

---
+++ _putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _putchar.c - ecrit le caractère c
+ * @c: le caractere
+ * 
+ * return - nothing
+ *
+ */
+
+int _putchar(char c)
+{  
+    return (write(1, %c, 1));
+}
