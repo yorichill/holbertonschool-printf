473259142aef9a25cb5e72b08459a006998bde08
zap9412 <thelyaan444@hotmail.com>
2025-11-26 10:04:04 +0100

    C - printf

--- _printf.c
+++ _printf.c
@@ -1,90 +1,88 @@
-/* _printf.c
- * Implémentation simplifiée de _printf qui gère : %c, %s et %%
- *
- * Contrainte : fichier compatible avec -std=gnu89 (déclarations en tête de bloc).
- */
-
-#include "main.h"    /* contient le prototype de _printf et autres helpers */
-#include <stdarg.h>
-#include <unistd.h>  /* pour write */
-#include <stddef.h>  /* pour NULL */
-
+#include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
 
 /**
-* _printf - Réplique simplifiée de printf
-* @format: Chaîne contenant du texte + directives de formatage
-*
-* Return: Nombre total de caractères imprimés, ou -1 en cas d'erreur
-*/
+ * _printf - version simplifiée de printf suivant le flowchart fourni
+ * @format: chaîne de format
+ *
+ * Return: nombre total de caractères imprimés, ou -1 en cas d'erreur
+ *
+ * Comportement:
+ *  - retourne -1 si format == NULL
+ *  - retourne -1 si un '%' est en fin de chaîne (format incorrect)
+ *  - gère les spécificateurs : %c, %s, %%
+ *  - si spécificateur inconnu -> imprime littéralement '%' suivi du caractère
+ */
 int _printf(const char *format, ...)
 {
-va_list args; /* Liste des arguments variables */
-int i = 0; /* Index dans la chaîne format */
-int printed_chars = 0; /* Compteur de caractères imprimés */
-int result; /* Résultat d'un handler */
+    va_list args;
+    int i = 0;
+    int printed_chars = 0;
+    int res;
+    int (*func)(va_list);
 
+    if (format == NULL)
+        return (-1);
 
-/* Vérification que format n'est pas NULL */
-if (format == NULL)
-return (-1);
+    va_start(args, format);
 
+    while (format[i] != '\0')
+    {
+        if (format[i] == '%')
+        {
+            i++; /* avancer au spécificateur (ou à '\0') */
 
-va_start(args, format); /* Initialisation de la liste variadique */
+            /* cas : '%' en fin de chaîne -> format invalide */
+            if (format[i] == '\0')
+            {
+                va_end(args);
+                return (-1);
+            }
 
+            /* récupérer la fonction correspondante au spécificateur */
+            func = get_specifier_func(format[i]);
 
-while (format[i] != '
-␀
-') /* Parcours de la chaîne */
-{
-if (format[i] == '%') /* Si on détecte une directive */
-{
-i++; /* On passe au caractère suivant qui indique le type */
+            if (func != NULL)
+            {
+                /* appel du handler */
+                res = func(args);
+                if (res < 0)
+                {
+                    va_end(args);
+                    return (-1);
+                }
+                printed_chars += res;
+            }
+            else
+            {
+                /* spécificateur inconnu : imprimer '%' puis le caractère */
+                if (write(1, "%", 1) != 1)
+                {
+                    va_end(args);
+                    return (-1);
+                }
+                if (write(1, &format[i], 1) != 1)
+                {
+                    va_end(args);
+                    return (-1);
+                }
+                printed_chars += 2;
+            }
+        }
+        else
+        {
+            /* caractère normal => affichage */
+            if (write(1, &format[i], 1) != 1)
+            {
+                va_end(args);
+                return (-1);
+            }
+            printed_chars++;
+        }
+        i++;
+    }
 
-
-if (format[i] == '
-␀
-')
-{
-va_end(args);
-return (-1); /* Format incorrect comme "%" seul */
-}
-
-
-/* Rechercher la fonction correspondant au type */
-int (*func)(va_list) = get_specifier_func(format[i]);
-
-
-if (func != NULL)
-{
-/* Appel de la fonction qui gère ce format */
-result = func(args);
-
-
-if (result == -1)
-{
-va_end(args);
-return (-1);
-}
-printed_chars += result;
-}
-else
-{
-/* Spécificateur inconnu : on affiche "%x" tel quel */
-write(1, "%", 1);
-write(1, &format[i], 1);
-printed_chars += 2;
-}
-}
-else
-{
-/* Sinon on imprime le caractère normalement */
-write(1, &format[i], 1);
-printed_chars++;
-}
-i++;
-}
-
-
-va_end(args); /* Libération de la liste variadique */
-return (printed_chars);
+    va_end(args);
+    return (printed_chars);
 }
