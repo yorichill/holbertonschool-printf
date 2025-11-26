27fbc40874bf6efd793412d8b846c4d1c4ed99c0
zap9412 <thelyaan444@hotmail.com>
2025-11-26 09:41:59 +0100

    C - printf

--- _printf.c
+++ _printf.c
@@ -9,49 +9,82 @@
 #include <unistd.h>  /* pour write */
 #include <stddef.h>  /* pour NULL */
 
+
 /**
- * _putchar - écrit un caractère sur stdout
- * @c: caractère à écrire
- *
- * Retourne : 1 si succès (nombre d'octets écrits),
- *            -1 si erreur d'écriture.
- *
- * Note : fonction bas-niveau centralisée pour détecter les erreurs d'écriture.
- */
-int _putchar(char c)
+* _printf - Réplique simplifiée de printf
+* @format: Chaîne contenant du texte + directives de formatage
+*
+* Return: Nombre total de caractères imprimés, ou -1 en cas d'erreur
+*/
+int _printf(const char *format, ...)
 {
-    if (write(1, &c, 1) != 1)
-        return (-1);
-    return (1);
+va_list args; /* Liste des arguments variables */
+int i = 0; /* Index dans la chaîne format */
+int printed_chars = 0; /* Compteur de caractères imprimés */
+int result; /* Résultat d'un handler */
+
+
+/* Vérification que format n'est pas NULL */
+if (format == NULL)
+return (-1);
+
+
+va_start(args, format); /* Initialisation de la liste variadique */
+
+
+while (format[i] != '
+␀
+') /* Parcours de la chaîne */
+{
+if (format[i] == '%') /* Si on détecte une directive */
+{
+i++; /* On passe au caractère suivant qui indique le type */
+
+
+if (format[i] == '
+␀
+')
+{
+va_end(args);
+return (-1); /* Format incorrect comme "%" seul */
 }
 
-/**
- * _puts - écrit une chaîne NUL-terminée sur stdout
- * @s: chaîne à écrire (si NULL, on affiche "(null)" par convention)
- *
- * Retourne : nombre total de caractères écrits, ou -1 si erreur d'écriture.
- *
- * Comportement :
- *  - si s == NULL on affiche "(null)" (comportement courant pour tests)
- *  - on utilise _putchar pour chaque caractère pour gérer les erreurs
- */
 
-int _puts(const char *s)
+/* Rechercher la fonction correspondant au type */
+int (*func)(va_list) = get_specifier_func(format[i]);
+
+
+if (func != NULL)
 {
-    int count = 0;
-    const char *p;
+/* Appel de la fonction qui gère ce format */
+result = func(args);
 
-    if (s == NULL)
-        p = "(null)";
-    else
-        p = s;
 
-    while (*p)
-    {
-        if (_putchar(*p) < 0)
-            return (-1);
-        count++;
-        p++;
-    }
-    return (count);
+if (result == -1)
+{
+va_end(args);
+return (-1);
+}
+printed_chars += result;
+}
+else
+{
+/* Spécificateur inconnu : on affiche "%x" tel quel */
+write(1, "%", 1);
+write(1, &format[i], 1);
+printed_chars += 2;
+}
+}
+else
+{
+/* Sinon on imprime le caractère normalement */
+write(1, &format[i], 1);
+printed_chars++;
+}
+i++;
+}
+
+
+va_end(args); /* Libération de la liste variadique */
+return (printed_chars);
 }
